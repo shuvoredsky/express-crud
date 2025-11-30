2c2812fac95110b972e9a043f9ac4a4632228b5f
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-11-30 23:39:35 +0600

    delete single user

--- src/server.ts
+++ src/server.ts
@@ -152,6 +152,33 @@ app.put("/users/:id", async(req:Request, res:Response)=>{
  }
 })
 
+
+app.delete("/users/:id", async(req:Request, res:Response)=>{
+ try{
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id])
+        if(result.rows.length === 0){
+            res.status(404).json({
+                 success: false,
+            message: "User not found",
+            })
+        }else{
+            res.status(200).json({
+                success: true,
+                message: "User fetched successfully",
+                data: result.rows[0],
+
+            })
+        }
+        
+ }catch(err: any){
+     res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        })  
+ }
+})
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
