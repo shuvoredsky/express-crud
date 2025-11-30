38ab51ddd3dc5f7c4bbf6e8c79cc264f0ab2380a
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-12-01 00:29:48 +0600

    get single todos

--- src/server.ts
+++ src/server.ts
@@ -179,6 +179,46 @@ app.delete("/users/:id", async(req:Request, res:Response)=>{
  }
 })
 
+
+//  todos crud
+app.post("/todos", async(req: Request, res:Response)=>{
+    const {user_id, title} = req.body;
+
+    try{
+        const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`, [user_id, title])
+        res.status(201).json({
+            success: true,
+            message: "Todo created",
+            data: result.rows[0]
+        })
+    }catch(err:any){
+        res.status(500).json({
+            success: false,
+
+        })
+    }
+})
+
+
+app.get("/todos", async(req: Request, res: Response)=>{
+    try{
+        const result = await pool.query(`SELECT * FROM todos`);
+
+        res.status(200).json({
+            success: true,
+            message: "todos retrieved successfully",
+            data: result.rows,
+        })
+
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        })
+    }
+})
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
